70e64383541344ba1183888827482dc22f93ee65
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-06-30 00:26:08 +0530

    Database Connected

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,18 +1,8 @@
 import mongoose from "mongoose";
 
 
-const connect = () => {
-    mongoose.connect(process.env.MONGODB_URI)
-        .then(() => {
-            console.log("MongoDB connected successfully");
-        })
-        .catch(err => {
-            console.log(err);
-        })
-}
-
 // const connect = () => {
-//     mongoose.connect('mongodb://0.0.0.0/SleekMind')
+//     mongoose.connect(process.env.MONGODB_URI)
 //         .then(() => {
 //             console.log("MongoDB connected successfully");
 //         })
@@ -20,5 +10,15 @@ const connect = () => {
 //             console.log(err);
 //         })
 // }
+
+const connect = () => {
+    mongoose.connect('mongodb://0.0.0.0/SleekMind')
+        .then(() => {
+            console.log("MongoDB connected successfully");
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
 
 export default connect;
