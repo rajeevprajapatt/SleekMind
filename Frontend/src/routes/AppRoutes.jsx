da8d83c1a90974a7250c633be4d6762aa56ee35e
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-06-30 00:30:07 +0530

    Routers created

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -3,16 +3,36 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from '../screen/login'
 import Register from '../screen/Register'
 import Home from '../screen/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <div>
+      <Home />
+    </div>
+  },
+  {
+    path: "/login", element: <div>
+      <Login />
+    </div>
+  },
+  {
+    path: "/register", element: <div>
+      <Register />
+    </div>
+  }
+])
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />}></Route>
+    //     <Route path="/login" element={<Login />}></Route>
+    //     <Route path="/register" element={<Register />}></Route>
+    //   </Routes>
+    // </BrowserRouter>
   )
 }
 
