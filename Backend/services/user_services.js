285a9c1e035dc717407cfd7725dc54f09905ecb3
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-07-02 23:41:17 +0530

    A service function created for fetching all users

--- Backend/services/user_services.js
+++ Backend/services/user_services.js
@@ -15,3 +15,10 @@ export const createUser = async ({ email, password }) => {
 
     return user;
 }
+
+export const getAllUsers = async ({ userId }) => {
+    const users = await User.find({
+        _id: { $ne: userId }
+    });
+    return users;
+}
