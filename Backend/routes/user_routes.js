8cd619e82b40f5c9810fb0aa9261fa6633cc81e3
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-07-02 23:39:50 +0530

    Router created for get all users

--- Backend/routes/user_routes.js
+++ Backend/routes/user_routes.js
@@ -21,4 +21,9 @@ router.get("/profile", authMiddleware.authUser, userController.profileUserContro
 
 router.get("/logout",authMiddleware.authUser,userController.logoutUserController);
 
+router.get("/all",
+    authMiddleware.authUser,
+    userController.getAllUsersController
+)
+
 export default router;
