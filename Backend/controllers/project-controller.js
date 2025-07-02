befd903791d9aa03b07ba3b29bd2fba36fde6a64
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-07-02 23:39:08 +0530

    contoller created for adding users to project

--- Backend/controllers/project-controller.js
+++ Backend/controllers/project-controller.js
@@ -16,11 +16,57 @@ export const createProject = async (req, res) => {
 
         const userId = loggedInUser._id;
 
+        console.log(userId);
+
         const newProject = await projectService.createProject({ name, userId });
 
         res.status(201).json(newProject);
     } catch (error) {
-        console.log(error); 
+        console.log(error);
         res.status(400).send(error.message);
     }
 }
+
+export const getAllProject = async (req, res) => {
+    try {
+        const loggedInUser = await User.findOne({
+            email: req.user.email
+        })
+
+        const allProjects = await projectService.getAllProjectByUserId({ userId: loggedInUser._id })
+
+        return res.status(200).json({ projects: allProjects })
+    } catch (error) {
+        console.log(err);
+        res.status(400).json({ error: err.message })
+    }
+}
+
+export const addUserToProject = async (req, res) => {
+    const errors = validationResult(req);
+
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const { projectId, users } = req.body;
+        const loggedInUser = await User.findOne({
+            email: req.user.email
+        })
+
+        const project = await projectService.addUsersToProjects({
+            projectId,
+            users,
+            userId: loggedInUser._id
+        })
+
+        return res.status(200).json({ project })
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ error: err.message })
+    }
+}
+
