e311bd02f3021dddbbc18b0579a814ebc7f31595
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-07-04 00:10:25 +0530

    fix: resolve navigate to dashboard

--- Frontend/src/screen/Login.jsx
+++ Frontend/src/screen/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
                 localStorage.setItem("token", response.data.token);
                 setUser(response.data.user);
 
-                navigate("/");
+                navigate("/dashboard");
             })
             .catch((error) => {
                 console.log(error.response)
