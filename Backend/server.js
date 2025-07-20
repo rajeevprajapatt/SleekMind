e235a440554fa0869546bd16249f8121d00ad0da
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-07-21 01:09:13 +0530

    feat: establish socket.io

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,8 @@ import app from "./app.js";
 import dotenv from "dotenv/config";
 import { Server } from 'socket.io';
 import jwt from 'jsonwebtoken'
+import mongoose from "mongoose";
+import projectModel from './models/project-model.js'
 
 
 const Port = process.env.PORT || 3000;
@@ -14,9 +16,17 @@ const io = new Server(server, {
     }
 });
 
-io.use((socket, next) => {
+io.use(async (socket, next) => {
     try {
         const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(' ')[1];
+        const projectId = socket.handshake.query.projectId;
+
+        if (!mongoose.Types.ObjectId.isValid(projectId)) {
+            return next(new Error('Invalid projectId'))
+        }
+
+        socket.project = await projectModel.findById(projectId);
+
 
         if (!token) {
             return next(new Error('Authorization error'));
@@ -39,6 +49,13 @@ io.use((socket, next) => {
 io.on('connection', socket => {
 
     console.log("a user connected");
+
+    socket.join(socket.project._id);
+
+    socket.on('project-message', data => {
+        socket.broadcast.to(socket.project._id).emit('project-message', data)
+    })
+
     socket.on('event', data => { /* … */ });
     socket.on('disconnect', () => { /* … */ });
 })
