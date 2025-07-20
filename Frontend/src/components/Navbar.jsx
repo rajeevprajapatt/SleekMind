736d2f3e3467dd1f634510a33ea5111d7fe4cdac
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-07-21 01:08:29 +0530

    Under progress

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from '../config/axios';
 
 const Navbar = () => {
+    const navigate = useNavigate();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
@@ -10,7 +13,15 @@ const Navbar = () => {
         else {
             setIsLoggedIn(false);
         }
-    })
+    }, [])
+
+    const handleLogOut = () => {
+        axios.get("/users/logout").then(response => console.log(response.data.message)).catch(error => console.log(error));
+        localStorage.removeItem("token")
+        localStorage.removeItem("user")
+        setIsLoggedIn(false)
+        navigate("/")
+    }
 
     return (
         <div className="bg-black/50 backdrop-blur-md text-white p-4 w-[98%] mx-auto rounded-xl shadow-md flex justify-between text-center items-center">
@@ -18,8 +29,8 @@ const Navbar = () => {
                 <h1 className="text-4xl font-bold px-8">Sleek Mind</h1>
             </div>
             <div className='flex items-center gap-4'>
-                {isLoggedIn ? <button className='bg-red-600 flex justify-center gap-1 text-lg p-2 px-4 rounded-md'>Log Out</button> :
-                    <button className='bg-blue-200 flex justify-center gap-1 text-lg p-2 px-4 rounded-md'> <i className="ri-user-line"></i>Login</button>
+                {isLoggedIn ? <button onClick={handleLogOut} className='bg-red-600 flex justify-center gap-1 text-lg p-2 px-4 rounded-md'>Log Out</button> :
+                    <button onClick={() => navigate("/login")} className='bg-blue-200 flex justify-center gap-1 text-lg p-2 px-4 rounded-md'> <i className="ri-user-line"></i>Login</button>
                 }
             </div>
         </div>
