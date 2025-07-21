90aa019884aca28ce778f2173e6f8904479540cf
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-07-22 01:17:36 +0530

    fix: user context issue fix

--- Frontend/src/context/user-context.jsx
+++ Frontend/src/context/user-context.jsx
@@ -6,7 +6,10 @@ export const UserContext = createContext();
 
 // Create a provider component
 export const UserProvider = ({children}) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(()=>{
+        const savedUser = localStorage.getItem("user");
+        return savedUser ? JSON.parse(savedUser) : null;
+    });
 
     return (
         <UserContext.Provider value = {{user, setUser}}>
