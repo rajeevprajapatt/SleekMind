164d372f83ba1fd27e57176e6d123f34898dc177
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-07-25 01:35:00 +0530

    fix: temporary remove morgan package

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 connect();
 
 app.use(cors());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
