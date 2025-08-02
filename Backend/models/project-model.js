234de8c74fba503f767dfb94b6fcabe7e82fe313
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-08-02 20:22:04 +0530

    refactor: removed unneccesory fields

--- Backend/models/project-model.js
+++ Backend/models/project-model.js
@@ -3,17 +3,16 @@ import mongoose from 'mongoose';
 const projectSchema = new mongoose.Schema({
     name: {
         type: String,
-        lowercase: true,
         required: true,
         trim: true,
-        unique: [true,"Project name must be unique"]
+        unique: [true, "Project name must be unique"]
     },
 
     users: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }]
-},{
+}, {
     timestamps: true
 })
 
