f718266adf11da2ea072c99c398d552d26f95103
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-08-02 20:16:12 +0530

    feat: controller created for message store

---
+++ Backend/controllers/chatMessage-controller.js
@@ -0,0 +1,17 @@
+import User from "../models/user.js";
+import ChatMessage from "../models/chat-message.js";
+import * as messageService from "../services/message-service.js";
+
+export const getAllChatMessages = async (req, res) => {
+    try {
+        const projectId = req.params.projectId;
+
+        const allMessages = await messageService.allProjectMessages(projectId);
+        console.log(projectId);
+        return res.status(201).json(allMessages);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send(error);
+    }
+}
