6cbb4b5ce6a4f0672c7bedd1249c425c5c6ddba1
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-08-02 20:19:29 +0530

    feat: define chat schema for message store

---
+++ Backend/models/chat-message.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema({
+    projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const ChatMessage = mongoose.model('ChatMessage', chatSchema)
+
+export default ChatMessage;
