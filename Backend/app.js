96cf9b7e744a66e80314441f5cbb92cc396b53d8
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-08-02 20:29:01 +0530

    feat: add AI route middleware

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,8 @@ import connect from "./database/db.js";
 import dotenv from "dotenv/config";
 import userRoutes from "./routes/user_routes.js"
 import projectRouter from './routes/project_routes.js';
+import chatRouter from './routes/chats_routes.js'
+import aiRouter from './routes/ai_routes.js'
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import ImageKit from "imagekit";
@@ -17,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+ 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.URL_ENDPOINT, 
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
@@ -29,6 +31,8 @@ app.get("/", (req, res) => {
 })
 app.use("/users", userRoutes);
 app.use("/projects", projectRouter);
+app.use("/chats",chatRouter);
+app.use("/ai", aiRouter);
 
 app.get("/imagekitAuth", (req, res) => {
   const {token,expire, signature} = imagekit.getAuthenticationParameters();
