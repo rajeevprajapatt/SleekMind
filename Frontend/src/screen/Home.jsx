fd58b4492c56c07d97b040358013d53b9befb921
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-08-02 20:38:25 +0530

    style: update ui colors

--- Frontend/src/screen/Home.jsx
+++ Frontend/src/screen/Home.jsx
@@ -8,35 +8,14 @@ const Home = () => {
   const { user } = useContext(UserContext);
   const navigate = useNavigate();
 
-  // If already logged in, redirect to /home
-  // useEffect(() => {
-  //   if (localStorage.getItem("token")) {
-  //     navigate("/dashboard");
-  //   }
-  //   else{
-  //     navigate("/");
-  //   }
-  // })
-
   return (
 
-    <div className='h-screen bg-gradient-to-br from-blue-500 to-purple-600'>
+    <div className='h-screen '>
       <div className="pt-1" />
       <Navbar />
       <div className="pt-1" />
       <div className="w-full h-[30rem] flex flex-col items-center justify-center bg-white text-white px-4">
         <img src={image}></img>
-          {/* 
-        <h1 className="text-4xl font-bold mb-4 text-center">Welcome to Project Manager</h1>
-        <p className="mb-6 text-lg text-center max-w-md">
-          Manage your teams and ideas in one powerful platform.
-        </p>
-        <button
-          onClick={() => navigate("/login")}
-          className="bg-white text-blue-700 px-6 py-2 rounded-md font-semibold hover:bg-blue-100 transition"
-        >
-          Login
-        </button> */}
       </div>
     </div>
 
