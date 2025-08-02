81d73f44da5b58db8b5fec428f98373ef6c6d99f
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-08-02 20:17:34 +0530

    fix: fixed getProjectById controller

--- Backend/controllers/project-controller.js
+++ Backend/controllers/project-controller.js
@@ -37,7 +37,7 @@ export const getAllProject = async (req, res) => {
 
         return res.status(200).json({ projects: allProjects })
     } catch (error) {
-        console.log(err);
+        console.log(error);
         res.status(400).json({ error: err.message })
     }
 }
