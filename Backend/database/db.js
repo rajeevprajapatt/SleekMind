318b60835214eba74280d5414fcda3e447eebfa4
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-08-17 01:53:00 +0530

    fix: ai user issue

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import User from "../models/user.js";
 
 
-const connect = () => {
+const connect = async () => {
     mongoose.connect(process.env.MONGODB_URI)
         .then(() => {
             console.log("MongoDB connected successfully");
@@ -9,6 +10,21 @@ const connect = () => {
         .catch(err => {
             console.log(err);
         })
+
+    const exists = await User.findOne({ email: "ai@yourapp.com" });
+    if (!exists) {
+        await User.create({
+            _id: new mongoose.Types.ObjectId("000000000000000000000001"),
+            fullName: "AI Assistant",
+            email: "ai@yourapp.com",
+            password: null,
+            role: "system",
+            isAI: true,
+        });
+        console.log("✅ AI user inserted");
+    } else {
+        console.log("⚡ AI user already exists");
+    }
 }
 
 // const connect = () => {
