1f80d954450651d1b17b8e0e3f957cd60bc2fa9b
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-08-18 22:31:12 +0530

    fix: bcrypt issue

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,5 +1,5 @@
 import mongoose, { mongo } from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
