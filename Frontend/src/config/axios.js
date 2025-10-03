34c67cbbeda04e9f3e482020291149fbff28f9bb
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-10-03 22:27:47 +0530

    fix: improve token handling in axios instance for better authorization

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+const token = localStorage.getItem('token');
+
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        ...(token && { Authorization: `Bearer ${token}` })
     }
-})
+});
 
 export default axiosInstance;
