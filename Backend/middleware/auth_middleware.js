57ca0e6a8bb37a5c6915f8ed94cdc49e15331bd7
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2025-10-31 10:35:58 +0530

    okk

--- Backend/middleware/auth_middleware.js
+++ Backend/middleware/auth_middleware.js
@@ -3,7 +3,7 @@ import redisClient from "../services/redis_service.js";
 
 export const authUser = async (req, res, next) => {
     try {
-        const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+        const token = req.headers.authorization.split(" ")[1] || req.cookies.token;
 
         if (!token) {
             return res.status(401).json({ error: "Unauthorized User" });
