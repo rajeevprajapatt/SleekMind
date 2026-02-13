9cb1f45857caa26c77852faf48d83737513e4139
Rajeev Prajapat <rajeevprajapat06@gmail.com>
2026-02-13 22:24:59 +0530

    feat: add preloader removal logic before rendering App component

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 import App from './App.jsx'
 import 'remixicon/fonts/remixicon.css'
 
+const preloader = document.getElementById("preloader");
+if (preloader) preloader.remove();
+
 createRoot(document.getElementById('root')).render(
-    <App/>
+    <App />
 )
